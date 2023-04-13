45dd522ba99bd514ca28ddc0a985e7882a3fdc71
nikmaxon <nikmakson99@gmail.com>
2023-04-13 10:18:18 +0300

    Main has made. Tests are passed

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,19 @@
+import pytest
+from src.item import Item
+
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+
+@pytest.fixture
+def item():
+    return Item("Смартфон", 10000, 20)
+
+
+def test_calculate_total_price(item):
+    assert item.calculate_total_price() == 200000
+
+
+def test_apply_discount(item):
+    item.pay_rate = 0.8
+    item.apply_discount()
+    assert item.price == 8000.0
